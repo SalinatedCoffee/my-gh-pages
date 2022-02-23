32d4066b7108b0c292f6dab7f9146507fe5a1063
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2022-02-22 21:42:16 -0600

    Add MD/GFM rendering

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import ReackMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 import content from '../contents/lorem_content.json'
 
 export default class Post extends React.Component {
     render() {
         return (
             <div className={this.props.styles.canvas}>
-                <h>{content.title}</h>
-                <p>{content.content}</p>
+                <h>{content[1].title}</h>
+                <p><ReackMarkdown children={content[1].content} remarkPlugins={[remarkGfm]}></ReackMarkdown></p>
 </div>
         )
     }
